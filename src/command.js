51a31b07ea740cff63a14844883394dcfb220847
Toru Nagashima <star.ctor@gmail.com>
2015-03-11 18:11:20 +0900

    Switch argv parser to subarg.
    For --transform option, we have to be able to parse nested arguments by
    square brackets.
    refs #1

--- src/command.js
+++ src/command.js
@@ -1,41 +1,78 @@
 /*eslint no-process-exit:0*/
-import {Command} from "commander";
+import {readFileSync} from "fs";
+import {join as joinPath} from "path";
+import subarg from "subarg";
 import Cpx from "./cpx";
 
-// Define command.
-const command =
-  new Command("cpx")
-    .version("1.0.0")
-    .usage("<source> <dest> [options]")
-    .description(`
-copy file globs, watching for changes.
+const HELP_TEXT = `
+Usage: cpx <source> <dest> [options]
 
-  <source>: target file globs.
-            e.g. src/**/*.{html,png,jpg}
-  <dest>:   a path of destination directory.
-            e.g. app
-`.trim())
-    .option("--clean", "clean files that matches <source> pattern in <dest> directory before copying.")
-    .option("-w, --watch", "watch for files that matches <source> pattern, then copy to <dest> on changed.")
-    .option("-v, --verbose", "print copied/removed files.")
-    .parse(process.argv);
+  Copy file globs, watching for changes.
 
-// Get arguments.
-const source = command.args[0];
-const outDir = command.args[1];
-const shouldClean = Boolean(command.clean);
-const isWatchMode = Boolean(command.watch);
-const verbose = Boolean(command.verbose);
+    <source>  A file glob of target files.
+              e.g. src/**/*.{html,png,jpg}
+    <dest>    A path of destination directory.
+              e.g. app
 
-// Validate.
-if (source == null || outDir == null || command.args.length > 2) {
-  command.outputHelp();
+Options:
+
+  -c, --clean     Clean files that matches <source> like pattern in <dest>
+                  directory before the first copying.
+  -h, --help      Print usage information
+  -v, --verbose   Print copied/removed files.
+  -V, --version   Print the version number
+  -w, --watch     Watch for files that matches <source>, and copy the file to
+                  <dest> every changing.
+`;
+
+const KNOWN_OPTIONS = new Set([
+  "_",
+  "c", "clean",
+  "h", "help",
+  "v", "verbose",
+  "V", "version",
+  "w", "watch"
+]);
+
+// Parse arguments.
+const args = subarg(process.argv.slice(2), {
+  boolean: ["clean", "help", "verbose", "version", "watch"],
+  alias: {c: "clean", h: "help", v: "verbose", V: "version", w: "watch"}
+});
+
+// Validate Options.
+{
+  const unknowns = Object.keys(args).filter(key => !KNOWN_OPTIONS.has(key));
+  if (unknowns.length > 0) {
+    console.error(`Unknown option(s): ${unknowns.join(", ")}`);
+    console.log(HELP_TEXT);
+    process.exit(1);
+  }
+}
+
+// Help/Version.
+if (args.help) {
+  console.log(HELP_TEXT);
+  process.exit(0);
+}
+if (args.version) {
+  console.log(
+    JSON.parse(readFileSync(joinPath(__dirname, "../package.json"))).version);
+  process.exit(0);
+}
+
+// Validate Arguments.
+const source = args._[0];
+const outDir = args._[1];
+if (source == null || outDir == null || args.length > 2) {
+  console.log(HELP_TEXT);
   process.exit(1);
 }
 
+//------------------------------------------------------------------------------
 // Main.
 let cpx = new Cpx(source, outDir);
-if (verbose) {
+if (args.verbose) {
   cpx.on("copy", e => {
     console.log(`Copied: ${e.srcPath} --> ${e.dstPath}`);
   });
@@ -44,8 +81,8 @@ if (verbose) {
   });
 }
 
-if (shouldClean) {
-  if (verbose) {
+if (args.clean) {
+  if (args.verbose) {
     console.log();
     console.log(`Clean: ${cpx.dest}`);
     console.log();
@@ -56,15 +93,15 @@ if (shouldClean) {
   catch (err) {
     console.error(`Failed to clean: ${err.message}.`);
   }
-  if (verbose) {
+  if (args.verbose) {
     console.log();
     console.log(`Copy: ${source} --> ${outDir}`);
     console.log();
   }
 }
 
-if (isWatchMode) {
-  if (verbose) {
+if (args.watch) {
+  if (args.verbose) {
     cpx.on("watch-ready", () => {
       console.log();
       console.log(`Be watching in ${cpx.base}`);
