b58107b9f3e6b5f7e6615450f4a96bbd564008e5
Toru Nagashima <star.ctor@gmail.com>
2015-03-15 03:26:02 +0900

    Use run-all

--- tools/watch.js
+++
@@ -1,8 +0,0 @@
-var exec = require("child_process").exec;
-var babel = exec("babel src --out-dir lib --watch --source-maps-inline");
-var mocha = exec("mocha test/*.js --compilers js:babel/register --timeout 5000 --watch --colors");
-
-babel.stdout.pipe(process.stdout);
-babel.stderr.pipe(process.stderr);
-mocha.stdout.pipe(process.stdout);
-mocha.stderr.pipe(process.stderr);
